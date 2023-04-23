3e335f30efc68e70e70372622a5c0d7df8087a68
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2023-04-23 13:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, render_template
 import os
+import pandas as pd
 from src import *
 
 
 app = Flask(__name__)
+# Specify the file path of the CSV file
+file_path = "titles.csv"
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv(file_path)
+# Display the first few rows of the DataFrame
+print(df.head())
 
 @app.route("/")
 def index_page():
-    return index_handler()
+    return index_handler(df)
 
 
 if __name__=='__main__':
