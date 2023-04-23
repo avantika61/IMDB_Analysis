19824e2ae353643e3fe191f24781d1d20e473056
UTKARSH SHUKLA <uttkarsh.shuklas1996@gmail.com>
2023-04-23 13:20:48 +0530

    Update index.py
    pie chart for movies and shows added

--- src/index.py
+++ src/index.py
@@ -1,4 +1,9 @@
 from flask import render_template
+import pandas as pd
 
-def index_handler():
-    return render_template('index.html')
+def index_handler(df):
+    movie_count = df[df["type"] == "MOVIE"].shape[0]
+    show_count = df[df["type"] == "SHOW"].shape[0]
+    movie_show_response = [{"type":"Movie","total":movie_count, "color":"#E50914"},{"type":"Show","total":show_count,"color":"#430000"}]
+    print(movie_show_response)
+    return render_template('index.html', movie_show_response=movie_show_response)
